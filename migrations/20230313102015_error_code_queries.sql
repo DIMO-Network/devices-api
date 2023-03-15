a5d9bc96eea2e327dd3853ac823e83c935812e7d
0xdev22 <doyin@dimo.zone>
2023-03-15 16:59:33 -0400

    Fix issues with PR

--- migrations/20230313102015_error_code_queries.sql
+++ migrations/20230313102015_error_code_queries.sql
@@ -10,7 +10,7 @@ CREATE TABLE devices_api.error_code_queries(
     updated_at           timestamptz not null default current_timestamp,
 
     PRIMARY KEY (id),
-    CONSTRAINT fkey_user_device_id FOREIGN KEY (error_code_queries_user_device_id_fkey) REFERENCES user_devices(id)
+    CONSTRAINT fkey_user_device_id FOREIGN KEY (error_code_queries_user_device_id_fkey) REFERENCES user_devices(id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
