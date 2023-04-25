618a5bb769fbf6c20d533b3f5600933d85f62f1d
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2023-04-25 12:52:48 -0400

    Table-only

--- migrations/20230417142942_add_beneficiary_addr_to_autopi_tbl.sql
+++ migrations/20230417142942_add_beneficiary_addr_to_autopi_tbl.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 SET search_path = devices_api, public;
 
-ALTER TABLE autopi_units ADD COLUMN beneficiary bytea
-    CONSTRAINT autopi_units_beneficiary_address_check CHECK (length(beneficiary) = 20) NOT VALID;
+ALTER TABLE autopi_units ADD COLUMN beneficiary bytea;
+ALTER TABLE autopi_units ADD CONSTRAINT autopi_units_beneficiary_address_check CHECK (length(beneficiary) = 20) NOT VALID;
 
 -- +goose StatementEnd
 
