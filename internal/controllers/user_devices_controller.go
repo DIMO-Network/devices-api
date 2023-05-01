2e2949f5c71eb01452c047f82fa7f368c764400d
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2023-05-01 14:47:35 -0400

    PLA-1371 Take over Smartcar auth code exchange (#90)
    * Take over auth code exchange
    
    Print more detail: code, request id, body
    
    * Add in both places, clean up comment
    
    * Exchange URL, client
    
    * Kill var

--- internal/controllers/user_devices_controller.go
+++ internal/controllers/user_devices_controller.go
@@ -532,7 +532,7 @@ func (udc *UserDevicesController) RegisterDeviceForUserFromVIN(c *fiber.Ctx) err
 func (udc *UserDevicesController) RegisterDeviceForUserFromSmartcar(c *fiber.Ctx) error {
 	const smartCarIntegrationID = "22N2xaPOq2WW2gAHBHd0Ikn4Zob"
 	userID := helpers.GetUserID(c)
-	localLog := udc.log.With().Str("user_id", userID).Logger()
+	localLog := udc.log.With().Str("userId", userID).Str("integrationId", smartCarIntegrationID).Logger()
 
 	reg := &RegisterUserDeviceSmartcar{}
 	if err := c.BodyParser(reg); err != nil {
@@ -550,7 +550,12 @@ func (udc *UserDevicesController) RegisterDeviceForUserFromSmartcar(c *fiber.Ctx
 	// call SC api with stuff and get VIN
 	token, err := udc.smartcarClient.ExchangeCode(c.Context(), reg.Code, reg.RedirectURI)
 	if err != nil {
-		localLog.Err(err).Msg("Failed to exchange authorization code with Smartcar.")
+		var scErr *services.SmartcarError
+		if errors.As(err, &scErr) {
+			localLog.Error().Msgf("Failed exchanging Authorization code. Status code %d, request id %s, and body `%s`.", scErr.Code, scErr.RequestID, string(scErr.Body))
+		} else {
+			localLog.Err(err).Msg("Failed to exchange authorization code with Smartcar.")
+		}
 		// This may not be the user's fault, but 400 for now.
 		return fiber.NewError(fiber.StatusBadRequest, "Failed to exchange authorization code with Smartcar.")
 	}
