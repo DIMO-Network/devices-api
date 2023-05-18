7572a2b95f136eaa94ff10a6c5957f2c8774a453
0xdev22 <doyin@dimo.zone>
2023-05-18 12:46:11 -0400

    Fix issues from PR comments

--- internal/controllers/nft_controller.go
+++ internal/controllers/nft_controller.go
@@ -236,7 +236,11 @@ func (nc *NFTController) GetDCNNFTImage(c *fiber.Ctx) error {
 
 	c.Set("Content-Type", "image/svg+xml")
 
-	nc.dcnTmpl.Execute(c, struct{ Name string }{dcn.Name.String})
+	err = nc.dcnTmpl.Execute(c, struct{ Name string }{dcn.Name.String})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- migrations/20230516033000_change_error_codes_format.sql
+++ migrations/20230516033000_change_error_codes_format.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 SET search_path = devices_api, public;
-
+ALTER TABLE devices_api.error_code_queries DROP COLUMN query_response;
 ALTER TABLE devices_api.error_code_queries ADD column codes_query_response jsonb;
 -- +goose StatementEnd
 
