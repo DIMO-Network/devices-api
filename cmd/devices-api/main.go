53778b7cbc5041ffee2e3d9d860a4ed670fb25eb
0xdev22 <doyin@dimo.zone>
2023-05-19 16:35:03 -0400

    Shorten errorCodes response property name and remove errorCodes column from db

--- cmd/devices-api/main.go
+++ cmd/devices-api/main.go
@@ -83,7 +83,7 @@ func main() {
 		}
 		startMonitoringServer(logger, &settings)
 		eventService := services.NewEventService(&logger, &settings, deps.getKafkaProducer())
-		// startDeviceStatusConsumer(logger, &settings, pdb, eventService)
+		startDeviceStatusConsumer(logger, &settings, pdb, eventService)
 		startCredentialConsumer(logger, &settings, pdb)
 		startTaskStatusConsumer(logger, &settings, pdb)
 		startWebAPI(logger, &settings, pdb, eventService, deps.getKafkaProducer(), deps.getS3ServiceClient(ctx), deps.getS3NFTServiceClient(ctx))
