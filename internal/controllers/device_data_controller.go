6e11beb5f0c297dbbfc67554a2ea9b2172a291ba
James Reategui <JamesReate@users.noreply.github.com>
2023-06-08 22:14:20 -0400

    PRO-1498 add more bad odo's (#127)
    * if odo less than 100 do not return as likely bad data
    
    * linter
    
    * add more invalid odos

--- internal/controllers/device_data_controller.go
+++ internal/controllers/device_data_controller.go
@@ -184,7 +184,13 @@ func findMostRecentSignal(udd models.UserDeviceDatumSlice, path string, highestF
 
 // isOdometerValid encapsulates logic to decide whether to return odometer
 func isOdometerValid(odometer float64) bool {
-	return odometer >= 100
+	if odometer <= 100 {
+		return false
+	}
+	if odometer == 65539 || odometer == 65538 {
+		return false
+	}
+	return true
 }
 
 // calculateRange returns the current estimated range based on fuel tank capacity, mpg, and fuelPercentRemaining and returns it in Kilometers
@@ -384,7 +390,7 @@ func (udc *UserDevicesController) QueryDeviceErrorCodes(c *fiber.Ctx) error {
 // @Router      /user/devices/{userDeviceID}/error-codes [get]
 func (udc *UserDevicesController) GetUserDeviceErrorCodeQueries(c *fiber.Ctx) error {
 	logger := helpers.GetLogger(c, udc.log)
-
+	
 	userDeviceID := c.Params("userDeviceID")
 
 	userDevice, err := models.UserDevices(
