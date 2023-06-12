003a26474ca62e50979538b9c40bbd1dea734038
Dylan Moreland <dylan@dimo.zone>
2023-06-12 10:00:37 -0400

    Wire things up

--- internal/services/ingest_registrar.go
+++ internal/services/ingest_registrar.go
@@ -16,28 +16,27 @@ import (
 const ingestAutoPiRegistrationTopic = "table.device.integration.autopi"
 const autoPiRegistrationEventType = "zone.dimo.device.integration.autopi.register"
 
-const ingestAftermarketDeviceRegistrationTopic = "table.aftermarket.device.integration"
+const aftermarketDeviceIntegrationTopic = "table.aftermarket.device.integration"
+const aftermarketDeviceIntegrationEventType = "zone.dimo.aftermarket.device.integration"
 
 //go:generate mockgen -source ingest_registrar.go -destination mocks/ingest_registrar_mock.go
 type IngestRegistrar interface {
 	Register(externalID, userDeviceID, integrationID string) error
 	Deregister(externalID, userDeviceID, integrationID string) error
+
+	Register2(data *AftermarketDeviceVehicleMapping) error
+	Deregister2(addr common.Address) error
 }
 
 func NewIngestRegistrar(producer sarama.SyncProducer) IngestRegistrar {
-	eventType := autoPiRegistrationEventType
-	ingestTopic := ingestAutoPiRegistrationTopic
-
-	return &ingestRegistrar{eventType: eventType, ingestTopic: ingestTopic, Producer: producer}
+	return &ingestRegistrar{Producer: producer}
 }
 
 // IngestRegistrar is an interface to the table.device.integration.smartcar/autopi topic, a
 // compacted Kafka topic keyed by Smartcar vehicle ID or autoPi Device ID. The ingest service needs to match
 // these IDs to our device IDs.
 type ingestRegistrar struct {
-	eventType   string
-	ingestTopic string
-	Producer    sarama.SyncProducer
+	Producer sarama.SyncProducer
 }
 
 type deviceIDLink struct {
@@ -52,7 +51,7 @@ func (s *ingestRegistrar) Register(externalID, userDeviceID, integrationID strin
 		Subject:     userDeviceID,
 		SpecVersion: "1.0",
 		Time:        time.Now(),
-		Type:        s.eventType,
+		Type:        autoPiRegistrationEventType,
 		Data: deviceIDLink{
 			DeviceID:   userDeviceID,
 			ExternalID: externalID,
@@ -63,7 +62,7 @@ func (s *ingestRegistrar) Register(externalID, userDeviceID, integrationID strin
 		return fmt.Errorf("failed to serialize JSON body: %w", err)
 	}
 	message := &sarama.ProducerMessage{
-		Topic: s.ingestTopic,
+		Topic: ingestAutoPiRegistrationTopic,
 		Key:   sarama.StringEncoder(externalID),
 		Value: sarama.ByteEncoder(valueb),
 	}
@@ -77,8 +76,49 @@ func (s *ingestRegistrar) Register(externalID, userDeviceID, integrationID strin
 
 func (s *ingestRegistrar) Deregister(externalID, _, _ string) error {
 	message := &sarama.ProducerMessage{
-		Topic: s.ingestTopic,
+		Topic: ingestAutoPiRegistrationTopic,
 		Key:   sarama.StringEncoder(externalID),
+		Value: nil, // Delete from compacted topic.
+	}
+	_, _, err := s.Producer.SendMessage(message)
+	if err != nil {
+		return fmt.Errorf("failed sending to Kafka: %w", err)
+	}
+
+	return nil
+}
+
+func (s *ingestRegistrar) Register2(data *AftermarketDeviceVehicleMapping) error {
+	value := shared.CloudEvent[AftermarketDeviceVehicleMapping]{
+		ID:          ksuid.New().String(),
+		Source:      "dimo/integration/" + data.AftermarketDevice.IntegrationID,
+		Subject:     data.Vehicle.UserDeviceID,
+		SpecVersion: "1.0",
+		Time:        time.Now(),
+		Type:        aftermarketDeviceIntegrationEventType,
+		Data:        *data,
+	}
+	valueb, err := json.Marshal(value)
+	if err != nil {
+		return fmt.Errorf("failed to serialize JSON body: %w", err)
+	}
+	message := &sarama.ProducerMessage{
+		Topic: aftermarketDeviceIntegrationTopic,
+		Key:   sarama.StringEncoder(data.AftermarketDevice.Address.Hex()), // Must be checksummed
+		Value: sarama.ByteEncoder(valueb),
+	}
+	_, _, err = s.Producer.SendMessage(message)
+	if err != nil {
+		return fmt.Errorf("failed sending to Kafka: %w", err)
+	}
+
+	return nil
+}
+
+func (s *ingestRegistrar) Deregister2(addr common.Address) error {
+	message := &sarama.ProducerMessage{
+		Topic: ingestAutoPiRegistrationTopic,
+		Key:   sarama.StringEncoder(addr.Hex()),
 		Value: nil, // Delete from compacted topic.
 	}
 	_, _, err := s.Producer.SendMessage(message)
@@ -95,9 +135,10 @@ type AftermarketDeviceVehicleMapping struct {
 }
 
 type AftermarketDeviceVehicleMappingAftermarketDevice struct {
-	Address common.Address `json:"address"`
-	Token   *big.Int       `json:"token"`
-	Serial  string         `json:"serial"`
+	Address       common.Address `json:"address"`
+	Token         *big.Int       `json:"token"`
+	Serial        string         `json:"serial"`
+	IntegrationID string         `json:"integrationId"`
 }
 
 type AftermarketDeviceVehicleMappingVehicle struct {
