1b27326c3e1a411d853fd134c7ed3ffdda5e55dc
James Reategui <jreate@me.com>
2023-06-24 20:45:02 -0400

    add migrations

---
+++ migrations/20230624170004_integrations_serial.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+SET search_path = devices_api, public;
+
+alter table user_device_api_integrations
+    rename column autopi_unit_id to hw_serial;
+
+alter table aftermarket_devices drop column autopi_device_id;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+SET search_path = devices_api, public;
+
+alter table user_device_api_integrations
+    rename column hw_serial to autopi_unit_id;
+
+alter table aftermarket_devices add column autopi_device_id text;
+-- +goose StatementEnd
