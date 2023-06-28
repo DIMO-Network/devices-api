de3e39d0a31630d0ead50c49aed1cfa26dcbc352
Dylan Moreland <dylan@dimo.zone>
2023-06-28 14:40:06 -0400

    Routes

--- cmd/devices-api/api.go
+++ cmd/devices-api/api.go
@@ -244,6 +244,9 @@ func startWebAPI(logger zerolog.Logger, settings *config.Settings, pdb db.Store,
 
 		sdAuth.Get("/mint/:integrationNode/:vehicleNode", syntheticController.GetSyntheticDeviceMintingPayload)
 		sdAuth.Post("/mint/:integrationNode/:vehicleNode", syntheticController.MintSyntheticDevice)
+
+		sdAuth.Get("/:syntheticDeviceNode}/burn", syntheticController.GetSyntheticDeviceBurnPayload)
+		sdAuth.Post("/:syntheticDeviceNode}/burn", syntheticController.BurnSyntheticDevice)
 	}
 
 	// Vehicle owner routes.
