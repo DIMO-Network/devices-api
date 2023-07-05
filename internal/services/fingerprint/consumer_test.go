68f7a508d4b84cc53456594b5abe02287df05a13
Allyson-English <allyson.english@gmail.com>
2023-07-05 10:25:38 -0600

    more tests

--- internal/services/fingerprint/consumer_test.go
+++ internal/services/fingerprint/consumer_test.go
@@ -248,6 +248,69 @@ func (s *ConsumerTestSuite) TestVinCredentialerHandler() {
 
 func (s *ConsumerTestSuite) TestSignatureValidation() {
 
+	cases := []struct {
+		Data      DeviceFingerprintCloudEvent
+		Signature string
+	}{
+		{
+			Data: DeviceFingerprintCloudEvent{
+				CloudEventHeaders: CloudEventHeaders{
+					ID:          "2RvhwjUbtoePjmXN7q9qfjLQgwP",
+					Subject:     "0x448cF8Fd88AD914e3585401241BC434FbEA94bbb",
+					Source:      "aftermarket/device/fingerprint",
+					SpecVersion: "1.0",
+					Time:        "2023-06-30T14:51:42.63507585Z",
+					Type:        "zone.dimo.aftermarket.device.fingerprint",
+				},
+				Data: Data{
+					CommonData: CommonData{
+						BatteryVoltage: 13.49,
+						Timestamp:      1688136702634,
+						RpiUptimeSecs:  39,
+					},
+					Vin:      "W1N2539531F907299",
+					Protocol: "7",
+				},
+			},
+			Signature: "7c31e54ddcffc2a548ccaf10ed64b7e4bdd239bbaa3e5f6dba41d3e4051d930b7fbdf184724c2fb8d3b2ac8ac82662d2ed74e881dd01c09c4b2a9b4e62ede5db1b",
+		},
+		{
+			Data: DeviceFingerprintCloudEvent{
+				CloudEventHeaders: CloudEventHeaders{
+					ID:          "2S9zmhIVydI2mjPjqxOFiV8WYwN",
+					Subject:     "0xa3CF0f4670F557D1F2f5d38F35645363c5c06f8d",
+					Source:      "aftermarket/device/fingerprint",
+					SpecVersion: "1.0",
+					Time:        "2023-07-05T16:15:44.829649462Z",
+					Type:        "zone.dimo.aftermarket.device.fingerprint",
+				},
+				Data: Data{
+					CommonData: CommonData{
+						BatteryVoltage: 13.43,
+						Timestamp:      1688573744829,
+						RpiUptimeSecs:  37,
+					},
+					Vin:      "4T1BF1FK5EU372595",
+					Protocol: "6",
+				},
+			},
+			Signature: "e6d4fb3b81c2c533d9aaa9eb6131f2e0492e7e83fae3462fe64604d45a6eafa178193b47a6621e1ce7c29a5bb95adf294bcb5bae41e1244a9021c7f6c4a071621b",
+		},
+	}
+
+	for _, c := range cases {
+		data, err := json.Marshal(c.Data)
+		require.NoError(s.T(), err)
+
+		v, err := validSignature(c.Signature, data)
+		require.NoError(s.T(), err)
+
+		require.Equal(s.T(), v, true)
+	}
+}
+
+func (s *ConsumerTestSuite) TestInvalidSignature() {
+
 	msg := DeviceFingerprintCloudEvent{
 		CloudEventHeaders: CloudEventHeaders{
 			ID:          "2RvhwjUbtoePjmXN7q9qfjLQgwP",
@@ -268,12 +331,11 @@ func (s *ConsumerTestSuite) TestSignatureValidation() {
 		},
 	}
 
-	signature := "7c31e54ddcffc2a548ccaf10ed64b7e4bdd239bbaa3e5f6dba41d3e4051d930b7fbdf184724c2fb8d3b2ac8ac82662d2ed74e881dd01c09c4b2a9b4e62ede5db1b"
+	signature := "7c31e54ddcffc2a548ccaf10ed64b7e4bdd239bbaa3e5f6dba41d3e4051d930b7fbdf184724c2fb8d3b2ac8ac82662d2ed74e881dd01c09c4b2a9b4e62ede5db1a"
 	data, err := json.Marshal(msg.Data)
 	require.NoError(s.T(), err)
 
 	v, err := validSignature(signature, data)
-	require.NoError(s.T(), err)
-
-	require.Equal(s.T(), v, true)
+	require.Equal(s.T(), err.Error(), "invalid signature recovery id")
+	require.Equal(s.T(), false, v)
 }
