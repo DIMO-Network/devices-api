0ef742b3af9f2d19efc2ad28d828e83b4b8e8124
James Reategui <jreate@me.com>
2023-07-14 09:38:54 -0400

    update tests and remove deprecated test

--- internal/controllers/device_data_controller_test.go
+++ internal/controllers/device_data_controller_test.go
@@ -10,12 +10,8 @@ import (
 	"time"
 
 	"github.com/DIMO-Network/device-definitions-api/pkg/grpc"
-	"github.com/stretchr/testify/require"
-	"github.com/tidwall/gjson"
-
 	"github.com/DIMO-Network/devices-api/internal/config"
 	"github.com/DIMO-Network/devices-api/internal/constants"
-	"github.com/DIMO-Network/devices-api/internal/middleware/owner"
 	"github.com/DIMO-Network/devices-api/internal/services"
 	mock_services "github.com/DIMO-Network/devices-api/internal/services/mocks"
 	"github.com/DIMO-Network/devices-api/internal/test"
@@ -25,184 +21,13 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/segmentio/ksuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 const migrationsDirRelPath = "../../migrations"
-
-func TestUserDevicesController_GetUserDeviceStatus(t *testing.T) {
-	// arrange global db and route setup
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	logger := zerolog.New(os.Stdout).With().
-		Timestamp().
-		Str("app", "devices-api").
-		Logger()
-
-	ctx := context.Background()
-	pdb, container := test.StartContainerDatabase(ctx, t, migrationsDirRelPath)
-	defer func() {
-		if err := container.Terminate(ctx); err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	deviceDefIntSvc := mock_services.NewMockDeviceDefinitionIntegrationService(mockCtrl)
-	deviceDefSvc := mock_services.NewMockDeviceDefinitionService(mockCtrl)
-	scClient := mock_services.NewMockSmartcarClient(mockCtrl)
-	scTaskSvc := mock_services.NewMockSmartcarTaskService(mockCtrl)
-	teslaSvc := mock_services.NewMockTeslaService(mockCtrl)
-	teslaTaskService := mock_services.NewMockTeslaTaskService(mockCtrl)
-	nhtsaService := mock_services.NewMockINHTSAService(mockCtrl)
-	autoPiIngest := mock_services.NewMockIngestRegistrar(mockCtrl)
-	deviceDefinitionIngest := mock_services.NewMockDeviceDefinitionRegistrar(mockCtrl)
-	autoPiTaskSvc := mock_services.NewMockAutoPiTaskService(mockCtrl)
-	natsSvc, natsServer, err := mock_services.NewMockNATSService(natsStreamName)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer natsServer.Shutdown()
-
-	usersClient := test.UsersClient{}
-
-	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &logger, deviceDefSvc, deviceDefIntSvc, &fakeEventService{}, scClient, scTaskSvc, teslaSvc, teslaTaskService, nil, nil, nhtsaService, autoPiIngest, deviceDefinitionIngest, autoPiTaskSvc, nil, nil, nil, nil, nil, nil, nil, natsSvc)
-	app := fiber.New()
-	app.Get("/user/devices/:userDeviceID/status", test.AuthInjectorTestHandler(testUserID), owner.UserDevice(pdb, &usersClient, &logger), c.GetUserDeviceStatus)
-	// todo re-work these tests since logic mostly outside, will need a mockgst
-	t.Run("GET - device status merge autopi and smartcar", func(t *testing.T) {
-		// arrange db, insert some user_devices
-		autoPiInteg := test.BuildIntegrationGRPC(constants.AutoPiVendor, 10, 0)
-		smartCarInt := test.BuildIntegrationGRPC(constants.SmartCarVendor, 0, 0)
-		dd := test.BuildDeviceDefinitionGRPC(ksuid.New().String(), "Ford", "Mach E", 2020, autoPiInteg)
-		ud := test.SetupCreateUserDevice(t, testUserID, dd[0].DeviceDefinitionId, nil, "", pdb)
-		const unitID = "431d2e89-46f1-6884-6226-5d1ad20c84d9"
-		const deviceID = "device123"
-		_ = test.SetupCreateAftermarketDevice(t, testUserID, unitID, func(s string) *string { return &s }(deviceID), pdb)
-		_ = test.SetupCreateUserDeviceAPIIntegration(t, unitID, deviceID, ud.ID, autoPiInteg.Id, pdb)
-		_ = test.SetupCreateUserDeviceAPIIntegration(t, unitID, deviceID, ud.ID, smartCarInt.Id, pdb)
-		// SC data setup to  older
-		smartCarData := models.UserDeviceDatum{
-			UserDeviceID: ud.ID,
-			Signals: null.JSONFrom([]byte(`{"oil": {"value": 0.6859999895095825, "timestamp": "2023-04-27T14:57:37Z"}, 
-				"range": {"value": 187.79, "timestamp": "2023-04-27T14:57:37Z"}, 
-				"tires": {"value":{"backLeft": 244, "backRight": 280, "frontLeft": 244, "frontRight": 252}, "timestamp": "2023-04-27T14:57:37Z"}, 
-				"charging": {"value":false, "timestamp": "2023-04-27T14:57:37Z"}, 
-				"latitude": {"value":33.675048828125, "timestamp": "2023-04-27T14:57:37Z"}, 
-				"odometer": {"value":195677.59375, "timestamp": "2023-04-27T14:57:37Z"}, 
-				"longitude": {"value":-117.85894775390625, "timestamp": "2023-04-27T14:57:37Z"}
-				}`)),
-			CreatedAt:           time.Now().Add(time.Minute * -5),
-			UpdatedAt:           time.Now().Add(time.Minute * -5),
-			LastOdometerEventAt: null.TimeFrom(time.Now().Add(time.Minute * -5)),
-			IntegrationID:       smartCarInt.Id,
-		}
-		err := smartCarData.Insert(ctx, pdb.DBS().Writer, boil.Infer())
-		assert.NoError(t, err)
-		// newer autopi data, expect to replace lat/long
-		autoPiData := models.UserDeviceDatum{
-			UserDeviceID: ud.ID,
-			Signals: null.JSONFrom([]byte(`{"latitude": { "value": 33.75, "timestamp": "2023-04-27T15:57:37Z" },
-				"longitude": { "value": -117.91, "timestamp": "2023-04-27T15:57:37Z" } }`)),
-			CreatedAt:     time.Now().Add(time.Minute * -1),
-			UpdatedAt:     time.Now().Add(time.Minute * -1),
-			IntegrationID: autoPiInteg.Id,
-		}
-		err = autoPiData.Insert(ctx, pdb.DBS().Writer, boil.Infer())
-		assert.NoError(t, err)
-
-		request := test.BuildRequest("GET", "/user/devices/"+ud.ID+"/status", "")
-		response, _ := app.Test(request)
-		body, _ := io.ReadAll(response.Body)
-
-		if assert.Equal(t, fiber.StatusOK, response.StatusCode) == false {
-			fmt.Println("response body: " + string(body))
-		}
-
-		snapshot := new(DeviceSnapshot)
-		err = json.Unmarshal(body, snapshot)
-		assert.NoError(t, err)
-
-		assert.Equal(t, 187.79, *snapshot.Range)
-		assert.Equal(t, false, *snapshot.Charging)
-		assert.Equal(t, 244.0, snapshot.TirePressure.BackLeft)
-		assert.Equal(t, 195677.59375, *snapshot.Odometer)
-		assert.Equal(t, 33.75, *snapshot.Latitude, "expected autopi latitude")
-		assert.Equal(t, -117.91, *snapshot.Longitude, "expected autopi longitude")
-		assert.Equal(t, "2023-04-27T15:57:37Z", snapshot.RecordUpdatedAt.Format(time.RFC3339))
-
-		//teardown
-		test.TruncateTables(pdb.DBS().Writer.DB, t)
-	})
-}
-
-func Test_sortBySignalValueDesc(t *testing.T) {
-	udd := models.UserDeviceDatumSlice{
-		&models.UserDeviceDatum{
-			UserDeviceID: "123",
-			Signals: null.JSONFrom([]byte(`{ "odometer": {
-    "value": 88164.32,
-    "timestamp": "2023-04-27T15:57:37Z"
-  }}`)),
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-			IntegrationID: "123",
-		},
-		&models.UserDeviceDatum{
-			UserDeviceID: "123",
-			Signals: null.JSONFrom([]byte(`{ "odometer": {
-    "value": 88174.32,
-    "timestamp": "2023-04-27T16:57:37Z"
-  }}`)),
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-			IntegrationID: "345",
-		},
-	}
-	// validate setup is ok
-	assert.Equal(t, 88164.32, gjson.GetBytes(udd[0].Signals.JSON, "odometer.value").Float())
-	assert.Equal(t, 88174.32, gjson.GetBytes(udd[1].Signals.JSON, "odometer.value").Float())
-	// sort and validate
-	sortBySignalValueDesc(udd, "odometer")
-	assert.Equal(t, 88174.32, gjson.GetBytes(udd[0].Signals.JSON, "odometer.value").Float())
-	assert.Equal(t, 88164.32, gjson.GetBytes(udd[1].Signals.JSON, "odometer.value").Float())
-}
-
-func Test_sortBySignalTimestampDesc(t *testing.T) {
-	udd := models.UserDeviceDatumSlice{
-		&models.UserDeviceDatum{
-			UserDeviceID: "123",
-			Signals: null.JSONFrom([]byte(`{ "odometer": {
-    "value": 88164.32,
-    "timestamp": "2023-04-27T15:57:37Z"
-  }}`)),
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-			IntegrationID: "123",
-		},
-		&models.UserDeviceDatum{
-			UserDeviceID: "123",
-			Signals: null.JSONFrom([]byte(`{ "odometer": {
-    "value": 88174.32,
-    "timestamp": "2023-04-27T16:57:37Z"
-  }}`)),
-			CreatedAt:     time.Now(),
-			UpdatedAt:     time.Now(),
-			IntegrationID: "345",
-		},
-	}
-	// validate setup is ok
-	assert.Equal(t, 88164.32, gjson.GetBytes(udd[0].Signals.JSON, "odometer.value").Float())
-	assert.Equal(t, 88174.32, gjson.GetBytes(udd[1].Signals.JSON, "odometer.value").Float())
-	// sort and validate
-	sortBySignalTimestampDesc(udd, "odometer")
-	assert.Equal(t, 88174.32, gjson.GetBytes(udd[0].Signals.JSON, "odometer.value").Float())
-	assert.Equal(t, "2023-04-27T16:57:37Z", gjson.GetBytes(udd[0].Signals.JSON, "odometer.timestamp").Time().Format(time.RFC3339))
-	assert.Equal(t, 88164.32, gjson.GetBytes(udd[1].Signals.JSON, "odometer.value").Float())
-}
 
 func TestUserDevicesController_calculateRange(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
@@ -234,7 +59,7 @@ func TestUserDevicesController_calculateRange(t *testing.T) {
 		DeviceAttributes:   attrs,
 	}, nil)
 
-	_ = NewUserDevicesController(&config.Settings{Port: "3000"}, nil, &logger, deviceDefSvc, nil, &fakeEventService{}, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil)
+	_ = NewUserDevicesController(&config.Settings{Port: "3000"}, nil, &logger, deviceDefSvc, nil, &fakeEventService{}, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil)
 	rge, err := calculateRange(ctx, deviceDefSvc, ddID, styleID, .7)
 	require.NoError(t, err)
 	require.NotNil(t, rge)
@@ -314,7 +139,7 @@ func TestUserDevicesController_QueryDeviceErrorCodes(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes", test.AuthInjectorTestHandler(testUserID), c.QueryDeviceErrorCodes)
 
@@ -388,7 +213,7 @@ func TestUserDevicesController_ShouldErrorOnTooManyErrorCodes(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes", test.AuthInjectorTestHandler(testUserID), c.QueryDeviceErrorCodes)
 
@@ -461,7 +286,7 @@ func TestUserDevicesController_ShouldErrorInvalidErrorCodes(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes", test.AuthInjectorTestHandler(testUserID), c.QueryDeviceErrorCodes)
 
@@ -530,7 +355,7 @@ func TestUserDevicesController_ShouldErrorOnEmptyErrorCodes(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes", test.AuthInjectorTestHandler(testUserID), c.QueryDeviceErrorCodes)
 
@@ -599,7 +424,7 @@ func TestUserDevicesController_ShouldStoreErrorCodeResponse(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes", test.AuthInjectorTestHandler(testUserID), c.QueryDeviceErrorCodes)
 
@@ -684,7 +509,7 @@ func TestUserDevicesController_GetUserDevicesErrorCodeQueries(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Get("/user/devices/:userDeviceID/error-codes", test.AuthInjectorTestHandler(testUserID), c.GetUserDeviceErrorCodeQueries)
 
@@ -760,7 +585,7 @@ func TestUserDevicesController_ClearUserDeviceErrorCodeQuery(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes/clear", test.AuthInjectorTestHandler(testUserID), c.ClearUserDeviceErrorCodeQuery)
 
@@ -847,7 +672,7 @@ func TestUserDevicesController_ErrorOnAllErrorCodesCleared(t *testing.T) {
 	}()
 
 	testUserID := "123123"
-	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil)
+	c := NewUserDevicesController(&config.Settings{Port: "3000"}, pdb.DBS, &mockDeps.logger, mockDeps.deviceDefSvc, mockDeps.deviceDefIntSvc, &fakeEventService{}, mockDeps.scClient, mockDeps.scTaskSvc, mockDeps.teslaSvc, mockDeps.teslaTaskService, nil, nil, mockDeps.nhtsaService, mockDeps.autoPiIngest, mockDeps.deviceDefinitionIngest, mockDeps.autoPiTaskSvc, nil, nil, nil, nil, mockDeps.openAISvc, nil, nil, nil)
 	app := fiber.New()
 	app.Post("/user/devices/:userDeviceID/error-codes/clear", test.AuthInjectorTestHandler(testUserID), c.ClearUserDeviceErrorCodeQuery)
 
