ae7b82fd2c959702636b44c0d313c8cdea5d9b3a
James Reategui <jreate@me.com>
2023-09-19 13:34:59 -0400

    tweaks to powertrain script

--- cmd/devices-api/populate_powertrain.go
+++ cmd/devices-api/populate_powertrain.go
@@ -90,6 +90,7 @@ func populateUSAPowertrain(ctx context.Context, logger *zerolog.Logger, pdb db.S
 	}
 
 	processFromDeviceDefinition := func(device *models.UserDevice) error {
+		// todo what about device style - i think that also gets powertrain more specific
 		dd, err := deviceDefSvc.GetDeviceDefinitionByID(ctx, device.DeviceDefinitionID)
 		if err != nil {
 			return err
@@ -99,43 +100,54 @@ func populateUSAPowertrain(ctx context.Context, logger *zerolog.Logger, pdb db.S
 		if err := device.Metadata.Unmarshal(md); err != nil {
 			return err
 		}
+		initialPowertrain := md.PowertrainType
+		proposedPowertrain := services.ICE
 
+		// get the powertrain from the device definition
 		if len(dd.DeviceAttributes) > 0 {
 			// Find device attribute (powertrain_type)
 			for _, item := range dd.DeviceAttributes {
 				if item.Name == constants.PowerTrainTypeKey {
-					powertrainType := services.ConvertPowerTrainStringToPowertrain(item.Value)
-					if md.PowertrainType != nil {
-						if !strings.EqualFold(powertrainType.String(), md.PowertrainType.String()) {
-							fmt.Println("------------------------------------------------")
-							fmt.Println(dd.Name)
-							fmt.Println("Current powertrain is different than what Device Definitions proposes:")
-							fmt.Println("Current:" + md.PowertrainType.String())
-							fmt.Println("Proposed:" + powertrainType.String())
-							fmt.Println("y/n accept proposed? [n]")
-							accept := "n"
-							_, _ = fmt.Scanln(accept)
-							if strings.ToLower(strings.TrimSpace(accept)) == "n" {
-								fmt.Println("leaving Powertrain as is.")
-								return nil
-							}
-							fmt.Println("updating Powertrain to proposed.")
-						}
-					}
-
-					md.PowertrainType = &powertrainType
-					if err := device.Metadata.Marshal(md); err != nil {
-						return err
-					}
-
-					fmt.Printf("Updating powertrain for %s to %s \n", dd.Name, md.PowertrainType.String())
-
+					proposedPowertrain = services.ConvertPowerTrainStringToPowertrain(item.Value)
 					break
 				}
 			}
 		} else {
-			fmt.Println("skipping - no powertrain found for: " + dd.Name)
+			fmt.Println("no attributes found, using default powertrain ICE for: " + dd.Name)
 		}
+		if initialPowertrain == &proposedPowertrain {
+			return nil // no need to update in this case
+		}
+		// find case where we have a mismatch and want user input
+		if initialPowertrain != nil {
+			if !strings.EqualFold(proposedPowertrain.String(), initialPowertrain.String()) {
+				fmt.Println("------------------------------------------------")
+				fmt.Println(dd.Name)
+				fmt.Println("Current powertrain is different than what Device Definitions proposes:")
+				fmt.Println("Current:" + md.PowertrainType.String())
+				fmt.Println("Proposed:" + proposedPowertrain.String())
+				fmt.Println("y/n accept proposed? [n]")
+				accept := "n"
+				_, err = fmt.Scanln(accept)
+				if err != nil {
+					fmt.Println("Error:", err)
+					return err
+				}
+				if strings.ToLower(strings.TrimSpace(accept)) == "n" {
+					fmt.Println("leaving Powertrain as is.")
+					return nil
+				}
+				fmt.Println("updating Powertrain to proposed.")
+				fmt.Println("------------------------------------------------")
+			}
+		}
+
+		md.PowertrainType = &proposedPowertrain
+		if err := device.Metadata.Marshal(md); err != nil {
+			return err
+		}
+
+		fmt.Printf("Updating powertrain for %s to %s. before: %s \n", dd.Name, md.PowertrainType.String(), initialPowertrain)
 
 		if _, err := device.Update(ctx, pdb.DBS().Writer, boil.Infer()); err != nil {
 			return err
