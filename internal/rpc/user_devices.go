d4b8fbbc5e008ef378e51fd2effeecd68faa6060
Eduardo Rodríguez <eduardo.rodriguezz@outlook.com>
2023-09-23 10:09:55 +0200

    [SM-122] Remove get all user devices valuations, moved to valuations-api (#211)
    * [SM-122] remove get all valuations grpc endpoint for explorer, being move to valuations-api
    
    * [SM-122] remove get all valuations grpc endpoint for explorer, being move to valuations-api
    
    * [SM-122] change to self-hosted runner
    
    ---------
    
    Co-authored-by: Eduardo Rodriguez <eduardorodriguez@Eduardos-MacBook-Pro.local>

--- internal/rpc/user_devices.go
+++ internal/rpc/user_devices.go
@@ -31,11 +31,6 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-// Euro to USD conversion rate, used for calculating the price of the device, hardcoded for now
-const (
-	euroToUsd float64 = 1.10
-)
-
 func NewUserDeviceRPCService(
 	dbs func() *db.ReaderWriter,
 	settings *config.Settings,
@@ -369,78 +364,6 @@ func (s *userDeviceRPCServer) GetUserDeviceByAutoPIUnitId(ctx context.Context, r
 	}
 
 	return result, nil
-}
-
-func (s *userDeviceRPCServer) GetAllUserDeviceValuation(ctx context.Context, _ *emptypb.Empty) (*pb.ValuationResponse, error) {
-
-	query := `select sum(evd.retail_price) as total_retail,
-					 sum(evd.vincario_price) as total_vincario
-					 from
-                             (
-								select distinct on (vin) vin, 
-														pricing_metadata, 
-														jsonb_path_query(evd.pricing_metadata, '$.retail.kelley.book')::decimal as retail_price,
-														jsonb_path_query(evd.vincario_metadata, '$.market_price.price_avg')::decimal as vincario_price,
-														created_at
-       							from external_vin_data evd 
-								order by vin, created_at desc
-							) as evd;`
-
-	queryGrowth := `select sum(evd.retail_price) as total_retail,
-					 sum(evd.vincario_price) as total_vincario
-					 from
-						(
-							select distinct on (vin) vin, 
-													pricing_metadata, 
-													jsonb_path_query(evd.pricing_metadata, '$.retail.kelley.book')::decimal as retail_price, 
-													jsonb_path_query(evd.vincario_metadata, '$.market_price.price_avg')::decimal as vincario_price,
-													created_at
-							from external_vin_data evd 
-							where created_at > current_date - 7
-							order by vin, created_at desc
-						) as evd;`
-
-	type Result struct {
-		TotalRetail   null.Float64 `boil:"total_retail"`
-		TotalVincario null.Float64 `boil:"total_vincario"`
-	}
-	var total Result
-	var lastWeek Result
-
-	err := queries.Raw(query).Bind(ctx, s.dbs().Reader, &total)
-	if err != nil {
-		s.logger.Err(err).Msg("Database failure retrieving total valuation.")
-		return nil, status.Error(codes.Internal, "Internal error.")
-	}
-
-	err = queries.Raw(queryGrowth).Bind(ctx, s.dbs().Reader, &lastWeek)
-	if err != nil {
-		s.logger.Err(err).Msg("Database failure retrieving last week valuation.")
-		return nil, status.Error(codes.Internal, "Internal error.")
-	}
-
-	totalValuation := total.TotalRetail.Float64 // 0 by default
-	growthPercentage := 0.0
-
-	if !total.TotalVincario.IsZero() {
-		totalValuation += total.TotalVincario.Float64 * euroToUsd
-	}
-
-	if totalValuation > 0 {
-		totalLastWeek := lastWeek.TotalRetail.Float64
-
-		if !lastWeek.TotalVincario.IsZero() {
-			totalLastWeek += lastWeek.TotalVincario.Float64 * euroToUsd
-		}
-		growthPercentage = (totalLastWeek / totalValuation) * 100
-	}
-
-	// todo: get an average valuation per vehicle, and multiply for whatever count of vehicles we did not get value for
-
-	return &pb.ValuationResponse{
-		Total:            float32(totalValuation),
-		GrowthPercentage: float32(growthPercentage),
-	}, nil
 }
 
 func (s *userDeviceRPCServer) GetAllUserDevice(req *pb.GetAllUserDeviceRequest, stream pb.UserDeviceService_GetAllUserDeviceServer) error {
