0c978baec975ba8c055362800d421030c8eb5dc4
Dylan Moreland <dylan@dimo.zone>
2023-10-11 19:32:26 -0400

    Remove space

--- internal/rpc/user_devices.go
+++ internal/rpc/user_devices.go
@@ -457,7 +457,6 @@ func (s *userDeviceRPCServer) deviceModelToAPI(ud *models.UserDevice) *pb.UserDe
 				IntegrationTokenId: iTkID,
 			}
 		}
-
 	}
 
 	for i, udai := range ud.R.UserDeviceAPIIntegrations {
