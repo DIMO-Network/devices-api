d69f056d1a699793266ecdef3453831fc728d6d8
Ricardo Humber <5209720+rhvivancoeffio@users.noreply.github.com>
2023-11-07 10:14:00 -0500

    SM-270 - fingerprint ingest (#224)
    * fingerprint ingest
    
    * Validate integration status
    
    ---------
    
    Co-authored-by: ricardo <chenton147@hotmail.com>

--- internal/appmetrics/counters.go
+++ internal/appmetrics/counters.go
@@ -70,4 +70,12 @@ var (
 		},
 		[]string{"method", "path", "status"},
 	)
+
+	FingerprintRequestCount = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "devices_api_fingerprint_request_count",
+			Help: "The total number of Fingerprint requests",
+		},
+		[]string{"protocol", "status"},
+	)
 )
