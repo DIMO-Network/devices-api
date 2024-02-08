7bc8bc9348092e96847b51dffb27773802c33153
Allyson-English <allyson.english@gmail.com>
2024-02-07 20:58:28 -0700

    wip- abi encode leaf before hashing

--- internal/services/attestation/attestation.go
+++ internal/services/attestation/attestation.go
@@ -3,11 +3,14 @@ package attestation
 import (
 	"context"
 	"encoding/json"
+	"reflect"
 	"time"
 
 	"github.com/DIMO-Network/devices-api/models"
 	"github.com/DIMO-Network/shared/db"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/segmentio/ksuid"
 	"github.com/tidwall/gjson"
 	"github.com/txaty/go-merkletree"
@@ -26,34 +29,58 @@ type leaf struct {
 }
 
 func (l *onChainAttestation) Serialize() ([]byte, error) {
-	return crypto.Keccak256(crypto.Keccak256(l.data)), nil
+	codeBytes, err := abiEncode([]interface{}{string(l.data)})
+	if err != nil {
+		return nil, err
+	}
+
+	return crypto.Keccak256(crypto.Keccak256(codeBytes)), nil
+}
+
+// TODO (ae): will this work for all types? need to check, might not want to infer
+func abiEncode(values []interface{}) ([]byte, error) {
+	argTypes := []reflect.Type{}
+	for _, val := range values {
+		argTypes = append(argTypes, reflect.TypeOf(val))
+	}
+
+	args := abi.Arguments{}
+	for _, argType := range argTypes {
+		abiType, err := abi.NewType(argType.Name(), "", nil)
+		if err != nil {
+			return nil, err
+		}
+		args = append(args, abi.Argument{Type: abiType})
+	}
+
+	return args.Pack(values...)
 }
 
 type attestor struct {
 	config mt.Config
 	pdb    db.Store
+	client *ethclient.Client
 }
 
-func New(pdb db.Store) *attestor {
+func New(client *ethclient.Client, pdb db.Store) *attestor {
 	return &attestor{
 		config: mt.Config{
 			SortSiblingPairs: true, // parameter for OpenZeppelin compatibility
 			HashFunc: func(data []byte) ([]byte, error) {
-				return crypto.Keccak256(crypto.Keccak256(data)), nil
+				return crypto.Keccak256(data), nil
 			},
 			Mode: mt.ModeProofGenAndTreeBuild,
 		},
-		pdb: pdb,
+		pdb:    pdb,
+		client: client,
 	}
 }
 
 func (m *attestor) Generate(ctx context.Context) (*merkletree.MerkleTree, error) {
-	valid, err := models.VerifiableCredentials(models.VerifiableCredentialWhere.ExpirationDate.GT(time.Now().Add(-7*time.Hour*24))).All(ctx, m.pdb.DBS().Reader)
+	valid, err := models.VerifiableCredentials(models.VerifiableCredentialWhere.ExpirationDate.GT(time.Now().Add(-30*time.Hour*24))).All(ctx, m.pdb.DBS().Reader)
 	if err != nil {
 		return nil, err
 	}
-
-	// sorting here?
 
 	blocks := []mt.DataBlock{}
 	for n, vc := range valid {
