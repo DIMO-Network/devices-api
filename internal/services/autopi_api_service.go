5840c0a205a85be77364316f4c04913d7ccc8016
James Reategui <JamesReate@users.noreply.github.com>
2024-03-06 16:17:12 -0500

    properly handle autopi not found error

--- internal/services/autopi_api_service.go
+++ internal/services/autopi_api_service.go
@@ -83,9 +83,11 @@ func (a *autoPiAPIService) GetUserDeviceIntegrationByUnitID(ctx context.Context,
 func (a *autoPiAPIService) GetDeviceByUnitID(unitID string) (*AutoPiDongleDevice, error) {
 	res, err := a.httpClient.ExecuteRequest(fmt.Sprintf("/dongle/devices/by_unit_id/%s/", unitID), "GET", nil)
 	if err != nil {
-		var htppResponseError shared.HTTPResponseError
-		if !errors.As(err, &htppResponseError) {
-			return nil, errors.Wrapf(err, "http resp error calling autopi api to get unit with ID %s", unitID)
+		var responseError shared.HTTPResponseError
+		if errors.As(err, &responseError) {
+			if responseError.StatusCode == 404 {
+				return nil, ErrNotFound
+			}
 		}
 		return nil, errors.Wrapf(err, "error calling autopi api to get unit with ID %s", unitID)
 	}
