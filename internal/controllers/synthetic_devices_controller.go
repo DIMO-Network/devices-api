57e975b54330ca2c18ded3a3176e447136c665b6
Dylan Moreland <79415431+elffjs@users.noreply.github.com>
2024-05-06 18:58:21 -0400

    SI-2601 Allow retries of failed transactions (#327)
    * Allow retrying vehicle minting
    
    Add missing load on vehicle burn query
    
    * Handling for aftermarket pair and unpair
    
    * VehicleMintRequest
    
    * SD minting
    
    * Swag
    
    * Use {var} instead of :var for Swagger comment path parameters
    
    * Get rid of a gratuitous load
    
    * Clean up paired/in pairing check
    
    * Whoops, missing return

--- internal/controllers/synthetic_devices_controller.go
+++ internal/controllers/synthetic_devices_controller.go
@@ -203,7 +203,7 @@ func (sdc *SyntheticDevicesController) MintSyntheticDevice(c *fiber.Ctx) error {
 
 	ud, err := models.UserDevices(
 		models.UserDeviceWhere.ID.EQ(userDeviceID),
-		qm.Load(models.UserDeviceRels.VehicleTokenSyntheticDevice),
+		qm.Load(qm.Rels(models.UserDeviceRels.VehicleTokenSyntheticDevice, models.SyntheticDeviceRels.MintRequest)),
 		qm.Load(models.UserDeviceRels.UserDeviceAPIIntegrations, models.UserDeviceAPIIntegrationWhere.IntegrationID.EQ(integrationID)),
 	).One(c.Context(), sdc.DBS().Reader)
 	if err != nil {
@@ -215,7 +215,14 @@ func (sdc *SyntheticDevicesController) MintSyntheticDevice(c *fiber.Ctx) error {
 	}
 
 	if ud.R.VehicleTokenSyntheticDevice != nil {
-		return fiber.NewError(fiber.StatusConflict, "Vehicle already paired with a synthetic device.")
+		if ud.R.VehicleTokenSyntheticDevice.R.MintRequest.Status != models.MetaTransactionRequestStatusFailed {
+			return fiber.NewError(fiber.StatusConflict, "Vehicle already paired with a synthetic device.")
+		}
+
+		_, err := ud.R.VehicleTokenSyntheticDevice.Delete(c.Context(), sdc.DBS().Writer)
+		if err != nil {
+			return fmt.Errorf("failed to delete failed synthetic minting: %v", err)
+		}
 	}
 
 	if len(ud.R.UserDeviceAPIIntegrations) == 0 {
