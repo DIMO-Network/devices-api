c9c09aaf90916e5429df812709d49dcb004b669f
Dylan Moreland <dylan@dimo.zone>
2024-05-07 00:00:23 -0400

    Regenerate error translation with new go vet coddling

--- internal/services/registry/error_translate.go
+++ internal/services/registry/error_translate.go
@@ -33,96 +33,111 @@ func (d *DimoRegistryErrorDecoder) Decode(data []byte) (string, error) {
 	}
 
 	switch selector := *(*[4]byte)(data[:4]); selector {
-	case [4]byte{209, 30, 53, 180}:
-		values, err := d.abi.Errors["AdNotPaired"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Aftermarket device %[1]s is not paired.", values...), nil
-	case [4]byte{205, 118, 232, 69}:
-		values, err := d.abi.Errors["DeviceAlreadyRegistered"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("There is already a minted device with address %[1]s.", values...), nil
-	case [4]byte{227, 202, 150, 57}:
-		values, err := d.abi.Errors["InvalidNode"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Token %[2]s does not exist at address %[1]s.", values...), nil
-	case [4]byte{82, 153, 186, 183}:
-		values, err := d.abi.Errors["InvalidParentNode"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Parent node %[1]s does not exist.", values...), nil
+	case [4]byte{219, 229, 56, 59}:
+		return "Invalid aftermarket device signature.", nil
+
+	case [4]byte{56, 168, 90, 141}:
+		return "Invalid owner signature.", nil
+
 	case [4]byte{248, 233, 93, 85}:
-		values, err := d.abi.Errors["InvalidSdSignature"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Invalid synthetic device signature.", values...), nil
-	case [4]byte{129, 94, 29, 100}:
-		values, err := d.abi.Errors["InvalidSigner"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Signer is owner of neither the vehicle nor the device.", values...), nil
-	case [4]byte{196, 106, 81, 104}:
-		values, err := d.abi.Errors["VehiclePaired"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Vehicle %[1]s is paired.", values...), nil
+		return "Invalid synthetic device signature.", nil
+
+	case [4]byte{79, 194, 128, 171}:
+		return "Vehicle and aftermarket device owners are not the same.", nil
+
 	case [4]byte{28, 72, 212, 158}:
 		values, err := d.abi.Errors["AttributeNotWhitelisted"].Inputs.Unpack(data[4:])
 		if err != nil {
 			return "", err
 		}
-		return fmt.Sprintf("Attribute %[1]s not whitelisted.", values...), nil
-	case [4]byte{77, 236, 136, 235}:
-		values, err := d.abi.Errors["DeviceAlreadyClaimed"].Inputs.Unpack(data[4:])
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Attribute %[1]s not whitelisted.", values[0]), nil
+	case [4]byte{205, 118, 232, 69}:
+		values, err := d.abi.Errors["DeviceAlreadyRegistered"].Inputs.Unpack(data[4:])
 		if err != nil {
 			return "", err
 		}
-		return fmt.Sprintf("Aftermarket device %[1]s already claimed.", values...), nil
-	case [4]byte{219, 229, 56, 59}:
-		values, err := d.abi.Errors["InvalidAdSignature"].Inputs.Unpack(data[4:])
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("There is already a minted device with address %[1]s.", values[0]), nil
+	case [4]byte{129, 94, 29, 100}:
+		return "Signer is owner of neither the vehicle nor the device.", nil
+
+	case [4]byte{227, 202, 150, 57}:
+		values, err := d.abi.Errors["InvalidNode"].Inputs.Unpack(data[4:])
 		if err != nil {
 			return "", err
 		}
-		return fmt.Sprintf("Invalid aftermarket device signature.", values...), nil
-	case [4]byte{56, 168, 90, 141}:
-		values, err := d.abi.Errors["InvalidOwnerSignature"].Inputs.Unpack(data[4:])
+		if l := len(values); l != 2 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 2", l)
+		}
+		return fmt.Sprintf("Token %[2]s does not exist at address %[1]s.", values[0], values[1]), nil
+	case [4]byte{82, 153, 186, 183}:
+		values, err := d.abi.Errors["InvalidParentNode"].Inputs.Unpack(data[4:])
 		if err != nil {
 			return "", err
 		}
-		return fmt.Sprintf("Invalid owner signature.", values...), nil
-	case [4]byte{79, 194, 128, 171}:
-		values, err := d.abi.Errors["OwnersDoNotMatch"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
 		}
-		return fmt.Sprintf("Vehicle and aftermarket device owners are not the same.", values...), nil
-	case [4]byte{21, 189, 170, 193}:
-		values, err := d.abi.Errors["AdNotClaimed"].Inputs.Unpack(data[4:])
-		if err != nil {
-			return "", err
-		}
-		return fmt.Sprintf("Aftermarket device %[1]s not claimed.", values...), nil
+		return fmt.Sprintf("Parent node %[1]s does not exist.", values[0]), nil
 	case [4]byte{118, 33, 22, 174}:
 		values, err := d.abi.Errors["AdPaired"].Inputs.Unpack(data[4:])
 		if err != nil {
 			return "", err
 		}
-		return fmt.Sprintf("Aftermarket device %[1]s is paired.", values...), nil
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Aftermarket device %[1]s is paired.", values[0]), nil
+	case [4]byte{77, 236, 136, 235}:
+		values, err := d.abi.Errors["DeviceAlreadyClaimed"].Inputs.Unpack(data[4:])
+		if err != nil {
+			return "", err
+		}
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Aftermarket device %[1]s already claimed.", values[0]), nil
 	case [4]byte{45, 145, 252, 181}:
 		values, err := d.abi.Errors["VehicleNotPaired"].Inputs.Unpack(data[4:])
 		if err != nil {
 			return "", err
 		}
-		return fmt.Sprintf("Vehicle %[1]s is not paired.", values...), nil
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Vehicle %[1]s is not paired.", values[0]), nil
+	case [4]byte{196, 106, 81, 104}:
+		values, err := d.abi.Errors["VehiclePaired"].Inputs.Unpack(data[4:])
+		if err != nil {
+			return "", err
+		}
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Vehicle %[1]s is paired.", values[0]), nil
+	case [4]byte{21, 189, 170, 193}:
+		values, err := d.abi.Errors["AdNotClaimed"].Inputs.Unpack(data[4:])
+		if err != nil {
+			return "", err
+		}
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Aftermarket device %[1]s not claimed.", values[0]), nil
+	case [4]byte{209, 30, 53, 180}:
+		values, err := d.abi.Errors["AdNotPaired"].Inputs.Unpack(data[4:])
+		if err != nil {
+			return "", err
+		}
+		if l := len(values); l != 1 {
+			return "", fmt.Errorf("unpacked into %d args instead of the expected 1", l)
+		}
+		return fmt.Sprintf("Aftermarket device %[1]s is not paired.", values[0]), nil
 	default:
 		return "", fmt.Errorf("unrecognized error selector %s", hexutil.Encode(selector[:]))
 	}
