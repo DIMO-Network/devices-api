e229b1c65822a47bb9867796a825c3a1f24fdb07
Dylan Moreland <dylan@dimo.zone>
2024-05-07 18:51:48 -0400

    Typo

--- internal/services/registry/error_translate.go
+++ internal/services/registry/error_translate.go
@@ -16,11 +16,11 @@ type ABIErrorTranslator struct {
 
 // NewABIErrorTranslator constructs a new error translator from the given ABI and
 // translation map. The keys of the map are names of errors in the ABI; the values
-// are Go templates using varables named after the arguments of the corresponding
+// are Go templates using variables named after the arguments of the corresponding
 // error.
 //
-// For example, if our ABI had an error TooPoor(address addr), then our translation map
-// could have the key "TooPoor" with value "Account {{ .addr }} is too poor.".
+// For example, if our ABI had an error TooPoor(address addr), then our translation
+// map could have the key "TooPoor" with value "Account {{ .addr }} is too poor.".
 func NewABIErrorTranslator(abi *abi.ABI, translation map[string]string) (*ABIErrorTranslator, error) {
 	m := make(map[string]*template.Template)
 
