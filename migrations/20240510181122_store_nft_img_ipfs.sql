bcff64632dd4dc07e6388505f375a2e8083aaac1
Allyson-English <allyson.english@gmail.com>
2024-05-10 18:26:08 -0400

    add col to user devices to store ipfs cid

---
+++ migrations/20240510181122_store_nft_img_ipfs.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- +goose StatementBegin
+SELECT 'up SQL query';
+-- +goose StatementEnd
+
+SET search_path = devices_api, public;
+ALTER TABLE user_devices ADD COLUMN ipfs_cid VARCHAR;
+
+-- +goose Down
+-- +goose StatementBegin
+SELECT 'down SQL query';
+
+SET search_path = devices_api, public;
+ALTER TABLE user_devices DROP COLUMN ipfs_cid;
+-- +goose StatementEnd
