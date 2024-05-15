a0d2d7103e28b79b5818b9d35a5fe813fecc100b
Dylan Moreland <dylan@dimo.zone>
2024-05-15 15:28:03 -0400

    Fix Goose migration structure

--- migrations/20240510181122_store_nft_img_ipfs.sql
+++ migrations/20240510181122_store_nft_img_ipfs.sql
@@ -1,15 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
--- +goose StatementEnd
-
 SET search_path = devices_api, public;
 ALTER TABLE user_devices ADD COLUMN ipfs_image_cid varchar;
+-- +goose StatementEnd
+
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
-
 SET search_path = devices_api, public;
 ALTER TABLE user_devices DROP COLUMN ipfs_image_cid;
 -- +goose StatementEnd
