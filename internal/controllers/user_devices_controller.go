c9791d49612f8776c9816181893e178415bf12fa
Allyson-English <allyson.english@gmail.com>
2024-05-21 14:25:30 -0400

    include dd before signing for mint w sd

--- internal/controllers/user_devices_controller.go
+++ internal/controllers/user_devices_controller.go
@@ -1583,20 +1583,6 @@ func (udc *UserDevicesController) PostMintDevice(c *fiber.Ctx) error {
 				return err
 			}
 
-			mvss := registry.MintVehicleAndSdSign{
-				IntegrationNode: new(big.Int).SetUint64(intID),
-			}
-
-			hash, err := client.Hash(&mvss)
-			if err != nil {
-				return err
-			}
-
-			sign, err := udc.wallet.SignHash(c.Context(), uint32(childNum), hash)
-			if err != nil {
-				return err
-			}
-
 			sd := models.SyntheticDevice{
 				IntegrationTokenID: types.NewDecimal(decimal.New(int64(intID), 0)),
 				MintRequestID:      requestID,
@@ -1609,6 +1595,19 @@ func (udc *UserDevicesController) PostMintDevice(c *fiber.Ctx) error {
 			}
 
 			if udc.Settings.IsProduction() {
+				mvss := registry.MintVehicleAndSdSign{
+					IntegrationNode: new(big.Int).SetUint64(intID),
+				}
+
+				hash, err := client.Hash(&mvss)
+				if err != nil {
+					return err
+				}
+
+				sign, err := udc.wallet.SignHash(c.Context(), uint32(childNum), hash)
+				if err != nil {
+					return err
+				}
 
 				return client.MintVehicleAndSdSign(requestID, contracts.MintVehicleAndSdInput{
 					ManufacturerNode:    makeTokenID,
@@ -1632,6 +1631,21 @@ func (udc *UserDevicesController) PostMintDevice(c *fiber.Ctx) error {
 						},
 					},
 				})
+			}
+
+			mvss := registry.MintVehicleAndSdWithDeviceDefinitionSign{
+				IntegrationNode:    new(big.Int).SetUint64(intID),
+				DeviceDefinitionID: dd.NameSlug,
+			}
+
+			hash, err := client.Hash(&mvss)
+			if err != nil {
+				return err
+			}
+
+			sign, err := udc.wallet.SignHash(c.Context(), uint32(childNum), hash)
+			if err != nil {
+				return err
 			}
 
 			if dd.NameSlug == "" {
