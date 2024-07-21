d9027892255c1c914ab457aa7ce1f7380311a02c
Dylan Moreland <dylan@dimo.zone>
2024-07-21 19:29:51 -0400

    Transactional. New job id

--- internal/controllers/user/sd/sd.go
+++ internal/controllers/user/sd/sd.go
@@ -1,10 +1,12 @@
 package sd
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/DIMO-Network/devices-api/internal/constants"
-	"github.com/DIMO-Network/devices-api/internal/controllers/helpers"
 	"github.com/DIMO-Network/devices-api/internal/middleware/address"
 	"github.com/DIMO-Network/devices-api/internal/services"
 	"github.com/DIMO-Network/devices-api/internal/services/integration"
@@ -42,18 +44,28 @@ type SyntheticTaskManager interface {
 // @Router /user/synthetic/device/{tokenID}/commands/reauthenticate [post]
 func (co *Controller) PostReauthenticate(c *fiber.Ctx) error {
 	userAddr := address.Get(c)
-	logger := helpers.GetLogger(c, nil)
+	// logger := helpers.GetLogger(c, nil)
 
 	tokenID, err := c.ParamsInt("tokenID")
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("Couldn't parse token id %q.", c.Params("tokenId")))
 	}
 
+	tx, err := co.DBS.DBS().Writer.BeginTx(c.Context(), &sql.TxOptions{Isolation: sql.LevelSerializable})
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
 	sd, err := models.SyntheticDevices(
 		models.SyntheticDeviceWhere.TokenID.EQ(types.NewNullDecimal(decimal.New(int64(tokenID), 0))),
 		qm.Load(models.SyntheticDeviceRels.VehicleToken),
-	).One(c.Context(), co.DBS.DBS().Reader)
+		qm.Load(models.SyntheticDeviceRels.BurnRequest),
+	).One(c.Context(), tx)
 	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return fiber.NewError(fiber.StatusNotFound, fmt.Sprintf("No synthetic device with token id %d known.", tokenID))
+		}
 		return err
 	}
 
@@ -67,11 +79,15 @@ func (co *Controller) PostReauthenticate(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusForbidden, "Caller is not the owner of this synthetic device.")
 	}
 
-	i, _ := sd.IntegrationTokenID.Int64()
+	if sd.R.BurnRequest != nil && sd.R.BurnRequest.Status != models.MetaTransactionRequestStatusFailed {
+		return fiber.NewError(fiber.StatusBadRequest, "Synthetic device is in the process of being burned.")
+	}
 
-	integ, _ := co.IntegClient.ByTokenID(c.Context(), int(i))
+	integTokenID, _ := sd.IntegrationTokenID.Int64()
 
-	udai, err := models.FindUserDeviceAPIIntegration(c.Context(), co.DBS.DBS().Reader, ud.ID, integ.ID)
+	integ, _ := co.IntegClient.ByTokenID(c.Context(), int(integTokenID))
+
+	udai, err := models.FindUserDeviceAPIIntegration(c.Context(), tx, ud.ID, integ.ID)
 	if err != nil {
 		return err
 	}
@@ -93,37 +109,47 @@ func (co *Controller) PostReauthenticate(c *fiber.Ctx) error {
 			return err
 		}
 
-		co.Store.Retrieve(c.Context(), userAddr)
-
 		err = co.Smartcar.StartPoll(udai, sd)
 		if err != nil {
 			return err
 		}
-
-		logger.Info().Int("sdId", tokenID).Msg("Restarted polling job.")
-
-		return c.JSON(Message{Message: "Restarted polling job."})
 	case constants.TeslaVendor:
 		cred, err := co.Store.Retrieve(c.Context(), userAddr)
 		if err != nil {
 			return err
 		}
 
-		if cred.IntegrationID != int(i) {
+		if cred.IntegrationID != int(integTokenID) {
 			return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("Stored credentials are for integration %d, not Tesla.", cred.IntegrationID))
 		}
 
-		_, err = co.TeslaAPI.GetVehicle(c.Context(), cred.AccessToken, "na", 1997)
+		teslaID, err := strconv.Atoi(udai.ExternalID.String)
 		if err != nil {
 			return err
 		}
 
+		// Make sure that these credentials have access to this particular vehicle.
+		_, err = co.TeslaAPI.GetVehicle(c.Context(), cred.AccessToken, "na", teslaID)
+		if err != nil {
+			return err
+		}
+
+		udai.Status = models.UserDeviceAPIIntegrationStatusPendingFirstData
+		udai.TaskID = null.StringFrom(ksuid.New().String())
+
 		// TODO(elffjs): Stop the old one, regenerate the id. Some races here.
-		co.Tesla.StartPoll(udai, sd)
-		return c.JSON(Message{Message: "Restarted polling job."})
+		if err := co.Tesla.StartPoll(udai, sd); err != nil {
+			return err
+		}
 	default:
-		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("Integration %s does not support reauthentication.", integ.Vendor))
+		return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("Integration %d does not support reauthentication.", integTokenID))
 	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return c.JSON(Message{Message: "Restarted polling job."})
 }
 
 type Message struct {
