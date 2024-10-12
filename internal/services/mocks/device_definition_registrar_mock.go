875254c7eee954e7caab0948d209b4de362e4cdf
James Reategui <JamesReate@users.noreply.github.com>
2024-10-11 22:13:18 -0400

    go generate

--- internal/services/mocks/device_definition_registrar_mock.go
+++ internal/services/mocks/device_definition_registrar_mock.go
@@ -5,7 +5,6 @@
 //
 //	mockgen -source device_definition_registrar.go -destination mocks/device_definition_registrar_mock.go
 //
-
 // Package mock_services is a generated GoMock package.
 package mock_services
 
--- internal/services/mocks/tesla_task_service_mock.go
+++ internal/services/mocks/tesla_task_service_mock.go
@@ -5,7 +5,6 @@
 //
 //	mockgen -source tesla_task_service.go -destination mocks/tesla_task_service_mock.go
 //
-
 // Package mock_services is a generated GoMock package.
 package mock_services
 
