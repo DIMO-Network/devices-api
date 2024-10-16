e3353b3945f22261d20d056b3fbe458581ff12b4
James Reategui <JamesReate@users.noreply.github.com>
2024-10-16 15:58:05 -0400

    regen some mocks manually help with ci action

--- internal/services/mocks/device_definitions_service_mock.go
+++ internal/services/mocks/device_definitions_service_mock.go
@@ -5,6 +5,7 @@
 //
 //	mockgen -source device_definitions_service.go -destination mocks/device_definitions_service_mock.go
 //
+
 // Package mock_services is a generated GoMock package.
 package mock_services
 
