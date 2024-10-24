38cc4e53a2789736e6d3130dbeae5d71070e14a1
James Reategui <JamesReate@users.noreply.github.com>
2024-10-24 19:10:33 -0400

    VIN patch endpoint add set the country code and protocol (#406)
    * set the country code and protocol, update docs
    
    * add test

--- internal/controllers/nft_controller_test.go
+++ internal/controllers/nft_controller_test.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"math/big"
-
 	"github.com/DIMO-Network/devices-api/internal/services/registry"
 	"github.com/DIMO-Network/devices-api/internal/test"
 	"github.com/DIMO-Network/devices-api/models"
@@ -20,6 +18,7 @@ import (
 	"github.com/volatiletech/null/v8"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"go.uber.org/mock/gomock"
+	"math/big"
 )
 
 func (s *UserDevicesControllerTestSuite) TestGetBurn() {
@@ -131,4 +130,39 @@ func (s *UserDevicesControllerTestSuite) TestPostBurn() {
 
 	s.Require().NotEmpty(ud.BurnRequestID)
 
+}
+
+func (s *UserDevicesControllerTestSuite) TestUpdateVINV2_setCountryAndProtocol() {
+	privKey, err := crypto.GenerateKey()
+	s.Require().NoError(err)
+	addr := crypto.PubkeyToAddress(privKey.PublicKey)
+	//email := "some@email.com"
+	//eth := addr.Hex()
+	dd := test.BuildDeviceDefinitionGRPC(ksuid.New().String(), "Ford", "Escape", 2020, nil)
+	s.deviceDefSvc.EXPECT().GetDeviceDefinitionByID(gomock.Any(), dd[0].DeviceDefinitionId).Return(dd[0], nil)
+
+	userDevice := test.SetupCreateUserDevice(s.T(), testUserID, dd[0].DeviceDefinitionId, nil, "", s.pdb)
+	_ = test.SetupCreateVehicleNFT(s.T(), userDevice, big.NewInt(1), null.BytesFrom(addr.Bytes()), s.pdb)
+
+	input := &UpdateVINReq{
+		VIN:         "4Y1SL65848Z411439",
+		CountryCode: "USA",
+		CANProtocol: "7",
+		Signature:   "",
+	}
+	marshal, _ := json.Marshal(input)
+	request := test.BuildRequest("PATCH", fmt.Sprintf("/vehicle/%s/vin", "1"), string(marshal))
+
+	response, err := s.app.Test(request)
+	s.Require().NoError(err)
+	s.Equal(204, response.StatusCode)
+
+	if err := userDevice.Reload(context.Background(), s.pdb.DBS().Reader); err != nil {
+		s.T().Fatal(err)
+	}
+
+	s.Equal("USA", userDevice.CountryCode.String)
+	s.Equal(`{"canProtocol": "7", "postal_code": null, "powertrainType": "ICE", "geoDecodedCountry": null, "geoDecodedStateProv": null}`,
+		string(userDevice.Metadata.JSON))
+	s.Equal("4Y1SL65848Z411439", userDevice.VinIdentifier.String)
 }
