356fb95cfaf67e42dfeaec01a2fb1cf6a2eb6506
Dylan Moreland <dylan@dimo.zone>
2025-02-11 12:18:45 -0500

    Delete burn test

--- internal/controllers/nft_controller_test.go
+++ internal/controllers/nft_controller_test.go
@@ -7,44 +7,11 @@ import (
 	"math/big"
 
 	"github.com/DIMO-Network/devices-api/internal/test"
-	"github.com/DIMO-Network/devices-api/models"
-	pb "github.com/DIMO-Network/shared/api/users"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/gofiber/fiber/v2"
 	"github.com/segmentio/ksuid"
 	"github.com/volatiletech/null/v8"
-	"github.com/volatiletech/sqlboiler/v4/boil"
 	"go.uber.org/mock/gomock"
 )
-
-func (s *UserDevicesControllerTestSuite) TestGetBurn() {
-	_, addr, err := test.GenerateWallet()
-	s.Require().NoError(err)
-
-	email := "some@email.com"
-	eth := addr.Hex()
-	ud := models.UserDevice{
-		ID:                 ksuid.New().String(),
-		UserID:             testUserID,
-		DeviceDefinitionID: ksuid.New().String(),
-		DefinitionID:       "ford_escape_2020",
-		CountryCode:        null.StringFrom("USA"),
-		Name:               null.StringFrom("Chungus"),
-		VinConfirmed:       true,
-		VinIdentifier:      null.StringFrom("4Y1SL65848Z411439"),
-	}
-
-	err = ud.Insert(context.Background(), s.pdb.DBS().Writer, boil.Infer())
-	s.Require().NoError(err)
-	test.SetupCreateVehicleNFT(s.T(), ud, big.NewInt(1), null.BytesFrom(addr.Bytes()), s.pdb)
-	user := test.BuildGetUserGRPC(ud.UserID, &email, &eth, &pb.UserReferrer{})
-	s.usersClient.EXPECT().GetUser(gomock.Any(), &pb.GetUserRequest{Id: ud.UserID}).Return(user, nil)
-
-	request := test.BuildRequest("GET", fmt.Sprintf("/vehicle/%s/commands/burn", "1"), "")
-	response, err := s.app.Test(request)
-	s.Require().NoError(err)
-	s.Equal(fiber.StatusOK, response.StatusCode)
-}
 
 func (s *UserDevicesControllerTestSuite) TestUpdateVINV2_setCountryAndProtocol() {
 	privKey, err := crypto.GenerateKey()
